af6e4e880085c600bb86f8ccd29ea324390f83cd
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2022-05-23 14:34:46 +0530

    Time: 9 ms (78.46%), Space: 9.5 MB (39.19%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
+    
+    int getPrefixSum(int x, int y, vector<vector<int>>& prefixSum)
+    {
+        int m = prefixSum.size();
+        int n = prefixSum[0].size(); 
+        if(x<0 || y<0)
+            return 0;
+        if(x>=m)
+            x=m-1;
+        if(y>=n)
+            y=n-1;
+        return prefixSum[x][y];
+    }
+    
+    
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) 
     {
         int m = mat.size();
         int n = mat[0].size();
+        vector<vector<int>> prefixSum(m,vector<int>(n,0));
+        
         vector<vector<int>> answer(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                for(int r=i-k;r<=i+k;r++)
-                {
-                    for(int c=j-k;c<=j+k;c++)
-                    {
-                        if(r>=0 && r<m && c>=0 && c<n)
-                            answer[i][j]+=mat[r][c];
-                    }
-                }
+                prefixSum[i][j]= mat[i][j]+ getPrefixSum(i-1,j,prefixSum)+getPrefixSum(i,j-1,prefixSum)-getPrefixSum(i-1,j-1,prefixSum);
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                answer[i][j]=getPrefixSum(i+k,j+k,prefixSum)+getPrefixSum(i-k-1,j-k-1,prefixSum)-getPrefixSum(i-k-1,j+k,prefixSum)-getPrefixSum(i+k,j-k-1,prefixSum);
             }
         }
         return answer;
+        
     }
 };
