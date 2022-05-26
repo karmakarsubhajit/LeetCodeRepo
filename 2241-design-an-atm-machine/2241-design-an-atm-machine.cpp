f26a9e50f3930bea02f280ae3e16b552f1fc6064
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2022-05-26 17:43:39 +0530

    Time: 1138 ms (5.01%), Space: 107.9 MB (79.61%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
@@ -1,51 +1,20 @@
-class ATM 
-{
-    public:
-        vector<long long int> amt{0,0,0,0,0};
-        vector<int> val{20,50,100,200,500};
-
-    ATM() 
-    {
-        
+class ATM {
+public:
+    long long bank[5] = {}, val[5] = {20, 50, 100, 200, 500};
+    void deposit(vector<int> banknotesCount) {
+        for (int i = 0; i < 5; ++i)
+            bank[i] += banknotesCount[i];
     }
-    
-    void deposit(vector<int> banknotesCount) 
-    {
-        for(int i=0;i<5;i++)
-        {
-            amt[i]+=banknotesCount[i];
+    vector<int> withdraw(int amount) {
+        vector<int> take(5);
+        for (int i = 4; i >= 0; --i) {
+            take[i] = min(bank[i], amount / val[i]);
+            amount -= take[i] * val[i];
         }
-    }
-    
-    vector<int> withdraw(int amount) 
-    {
-        int i = 4;
-        vector<long long int> temp = amt;
-        vector<int> ans(5,0);
-        while(amount>0 && i>=0)
-        {
-            if(amt[i]!=0)
-            {
-                int c= amount/val[i];
-                if(amt[i]<c)
-                    c=amt[i];
-                
-                amount-=c*val[i];
-                amt[i]-=c;
-                ans[i]=c;
-            }
-            i--;
-        }
-        if(amount==0)
-            return ans;
-        amt=temp;
-        return {-1};
+        if (amount)
+            return { -1 };
+        for (int i = 0; i < 5; ++i)
+            bank[i] -= take[i];            
+        return take;
     }
 };
-
-/**
- * Your ATM object will be instantiated and called as such:
- * ATM* obj = new ATM();
- * obj->deposit(banknotesCount);
- * vector<int> param_2 = obj->withdraw(amount);
- */
