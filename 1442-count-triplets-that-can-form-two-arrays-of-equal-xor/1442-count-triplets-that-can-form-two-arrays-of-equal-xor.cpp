035c6babaa15559aab3b6fa6aaff622df9707b86
karmakarsubhajit <46123595+karmakarsubhajit@users.noreply.github.com>
2022-05-27 14:15:44 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (36.71%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -5,22 +5,20 @@ public:
     {
         int n = arr.size();
         int ans = 0;
-        vector<int> pref(n+1,0);
+        
         unordered_map<int,int> cnt;
         unordered_map<int,int> tot;
         
-        
-        for(int i=1;i<=n;i++)
-        {
-            pref[i] = pref[i-1]^arr[i-1];
-        }
-        
-        
+        int pref = 0;
         for(int i=0;i<=n;i++)
         {
-            ans+=cnt[pref[i]]*(i-1)-tot[pref[i]];
-            cnt[pref[i]]++;
-            tot[pref[i]]+=i;
+            if(i==0)
+                pref=0;
+            else
+                pref = pref^arr[i-1];
+            ans+=cnt[pref]*(i-1)-tot[pref];
+            cnt[pref]++;
+            tot[pref]+=i;
         }
         
         return ans;
